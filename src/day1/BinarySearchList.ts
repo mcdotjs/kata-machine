887c5d5749090455b97e9c06918831367c322937
mcdotjs <mirectm@gmail.com>
2025-04-23 10:01:30 +0200

    binary search, crystall balls

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+  let high = haystack.length
+  let low = 0
+  while (low < high) {
+    let middleIndex = Math.floor(low + (high - low) / 2)
+    console.log({ low, high, middleIndex })
+    if (haystack[middleIndex] == needle) {
+      return true
+    } else if (needle > haystack[middleIndex]) {
+      //drop the midd point
+      low = middleIndex + 1
+    } else {
+      high = middleIndex
+    }
+  }
+  return false
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  let jumpAmount = Math.floor(Math.sqrt(breaks.length))
+  let i = jumpAmount
+  //i growing + jumpAmaount
+  for (; i < breaks.length; i += jumpAmount) {
 
+    if (breaks[i]) {
+      break
+    }
+  }
+  // going backwards
+  i -= jumpAmount
+  //i,j growing 1
+  for (let j = 0; j <= jumpAmount; ++j, ++i) {
+    if (breaks[i]) {
+      return i
+    }
+  }
+  return -1
 }
