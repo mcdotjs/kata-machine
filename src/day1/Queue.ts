f4d312e678370f4644412eadca1298498ffd49da
mcdotjs <mirectm@gmail.com>
2025-04-23 22:55:48 +0200

    q1

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,25 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head: Node<T> | undefined
+  private tail?: Node<T>
 
-    
 
-    constructor() {
-    }
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0
+  }
 
-    enqueue(item: T): void {
+  enqueue(item: T): void {
 
-}
-    deque(): T | undefined {
+  }
+  deque(): T | undefined {
 
-}
-    peek(): T | undefined {
+  }
+  peek(): T | undefined {
 
-}
+  }
 }
