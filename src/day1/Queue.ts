9f73890cc48300b546987583c8bc7a262dfec2f9
mcdotjs <mirectm@gmail.com>
2025-04-25 10:14:22 +0200

    queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -14,12 +14,32 @@ export default class Queue<T> {
   }
 
   enqueue(item: T): void {
+    this.length++
+    const n = { value: item } as Node<T>
+    if (!this.tail || !this.head) {
+      this.head = this.tail = n
+      return
+    }
+    // keby si toto nedal tak stratis data.. prepises ich
+    this.tail.next = n
+    // az ked pripojis dasiu node, tak az postom updatujes pointer na tail
+    this.tail = n
+
 
   }
   deque(): T | undefined {
+    if (!this.head) {
+      return undefined
+    }
+
+    this.length--
+    const h = this.head
+    this.head = this.head.next
+    h.next = undefined
+    return h.value
 
   }
   peek(): T | undefined {
-
+    return this.head?.value
   }
 }
