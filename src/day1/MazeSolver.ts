95d687082a1e1dfe136299c90aa5c4718c4709ce
mcdotjs <mirectm@gmail.com>
2025-05-05 09:23:35 +0200

    quick sort

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -6,7 +6,6 @@ const dir = [
 ]
 function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
   //basecases
-  
   // of the map
   if (curr.x < 0 || curr.x >= maze[0].length || curr.y < 0 || curr.y >= maze.length) {
     return false
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,41 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
+  //console.log(arr)
+  // base case ... when low meets high
+  if (lo >= hi) {
+    return;
+  }
+  //this thind is gonna do weak sort
+  //is gonna set the pivot into the correct, then return pivots index
+  const pivotIdx = partition(arr, lo, hi)
+  // IT'S INCLUSIVE ENDIGS
+  qs(arr, lo, pivotIdx - 1)
+  qs(arr, pivotIdx + 1, hi)
 
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+  const pivot = arr[hi]
+  // why -1... this is weakly sorting thing (????)
+  let idx = lo - 1
+
+  //NOTE: walk form the low to the hi, but not including hi
+  // this is weak sort
+  // each element has to be compared to the pivot
+
+  for (let i = lo; i < hi; ++i) {
+    if (arr[i] <= pivot) {
+      idx++
+      const temp = arr[i]
+      arr[i] = arr[idx]
+      arr[idx] = temp
+    }
+  }
+  idx++
+  arr[hi] = arr[idx]
+  arr[idx] = pivot
+  return idx
+}
+
+export default function quick_sort(arr: number[]): void {
+  qs(arr, 0, arr.length - 1)
 }
