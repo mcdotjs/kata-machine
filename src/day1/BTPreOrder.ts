271e5ac2e445f4afb5d15b9ee98997cedeeac5fc
mcdotjs <mirectm@gmail.com>
2025-05-05 17:54:39 +0200

    binary tree traversal pre order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,30 @@
+function walk(n: BinaryNode<number> | null, path: number[]): number[] {
+  //base
+  if (!n) {
+    return path
+  }
+
+  //recurse
+  //pre
+  path.push(n.value)
+  //NOTE: ked dam prec z param null type
+  // if (n.left) {
+  
+  // recurse
+  walk(n.left, path)
+  //}
+
+  //if (n.right) {
+  walk(n.right, path)
+  //}
+  
+  //post
+  return path
+}
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+  let arr = [] as number[]
+  walk(head, arr)
+
+  return arr
 
 }
