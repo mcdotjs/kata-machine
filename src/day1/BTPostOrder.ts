b04fb6d703b7bc4c05253a5bb921cba45b0bd3b5
mcdotjs <mirectm@gmail.com>
2025-05-05 17:59:33 +0200

    binary tree post order traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,30 @@
+function walk(n: BinaryNode<number> | null, path: number[]): number[] {
+  //base
+  if (!n) {
+    return path
+  }
+
+  //recurse
+  //pre
+  //NOTE: ked dam prec z param null type
+  // if (n.left) {
+
+  // recurse
+  walk(n.left, path)
+  //}
+
+  //if (n.right) {
+  walk(n.right, path)
+  //}
+
+  path.push(n.value)
+  //post
+  return path
+}
 export default function post_order_search(head: BinaryNode<number>): number[] {
+  let arr = [] as number[]
+  walk(head, arr)
+
+  return arr
 
 }
